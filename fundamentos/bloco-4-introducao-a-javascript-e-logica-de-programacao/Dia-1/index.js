882c3c94ac39ba18547c08eac345b15588ac67e4
Jessy Cristina <jessyaraujo395@gmail.com>
2022-03-07 18:49:56 -0300

    parte 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/Dia-1/index.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/Dia-1/index.js
@@ -1,11 +1,18 @@
 const a = 2;
 const b = 5;
 
-let soma = a + b;
-let subtracao = a - b;
-let multiplicacao = a * b;
-let divisao = a / b;
-let modulo = a % b;
+// let soma = a + b;
+// let subtracao = a - b;
+// let multiplicacao = a * b;
+// let divisao = a / b;
+// let modulo = a % b;
 
-console.log(soma, subtracao, multiplicacao, divisao, modulo);
+// console.log(soma, subtracao, multiplicacao, divisao, modulo);
 
+// parte 2
+
+if (a > b) {
+    console.log(a);
+} else {
+    console.log(b);
+}
